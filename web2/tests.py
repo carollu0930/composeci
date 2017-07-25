262d089274fcc30042a04a45d6761157e8cf197c
lujuan0930 <Lujuan_0930@163.com>
2017-07-25 15:25:08 +0800

    temp test

---
+++ web2/tests.py
@@ -0,0 +1,17 @@
+import unittest
+from app import app
+
+
+class MainTestCase(unittest.TestCase):
+
+    def test_two_and_two(self):
+        four = 2 + 2
+        self.assertEqual(four, 4)
+        self.assertNotEqual(four, 5)
+        self.assertNotEqual(four, 6)
+        self.assertNotEqual(four, 22)
+        self.assertNotEqual(four, 66)
+
+
+if __name__ == '__main__':
+    unittest.main()
