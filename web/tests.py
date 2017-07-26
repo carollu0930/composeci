c56054f7cfd5023cfc7838d55c069064159dd37a
lujuan0930 <Lujuan_0930@163.com>
2017-07-26 14:50:01 +0800

    circle-test

--- web/tests.py
+++ web/tests.py
@@ -10,7 +10,7 @@ class MainTestCase(unittest.TestCase):
         self.assertNotEqual(four, 5)
         self.assertNotEqual(four, 6)
         self.assertNotEqual(four, 22)
-        self.assertNotEqual(four, 66)
+        self.assertNotEqual(four, 661)
 
 
 if __name__ == '__main__':
